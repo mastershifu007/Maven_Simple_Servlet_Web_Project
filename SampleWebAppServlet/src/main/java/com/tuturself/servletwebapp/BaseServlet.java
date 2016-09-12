fd4ef759e008ca8480294daf226715bb9e162038
Master Shifu <majinbusaga@gmail.com>
2016-09-12 15:47:41 +0530

    Initial Commit

---
+++ SampleWebAppServlet/src/main/java/com/tuturself/servletwebapp/BaseServlet.java
@@ -0,0 +1,42 @@
+package com.tuturself.servletwebapp;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class BaseServlet
+ */
+
+public class BaseServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BaseServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+	    request.setAttribute("serverTime", new Date().toString());
+	    request.getRequestDispatcher("/serverTime.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ SampleWebAppServlet/src/main/webapp/serverTime.jsp
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Home Page</title>
+</head>
+<body>
+<h1>Hello World!! Welcome to Tuturself</h1>
+Server Time : <%=(String)request.getAttribute("serverTime")%>
+
+</body>
+</html>
